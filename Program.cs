8422506b7ef1e32d48615c6ad30984b99337bcc4
Frederic Forjan <GeoVah@freds-MacBook-Pro.local>
2015-12-02 12:54:00 -0800

    Back to english

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace LunchLearn
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello Le monde");
+            Console.WriteLine("Hello World");
         }
     }
 }
