d6895bedc6accc6414d7502ecbb0caa8cd777ae7
Frederic Forjan <GeoVah@freds-MacBook-Pro.local>
2015-12-02 12:53:42 -0800

    initial import

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace LunchLearn
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Le monde");
+        }
+    }
+}
